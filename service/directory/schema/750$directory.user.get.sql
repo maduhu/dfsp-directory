675ffbb17bbfb15e434706c1581c7e3deeaa251e
stamen.peev <stamen.peev@softwaregroup-bg.com>
2016-11-14 14:54:09 +0200

    feat: add actorId as an extra argument to directory.user.get

--- service/directory/schema/750$directory.user.get.sql
+++ service/directory/schema/750$directory.user.get.sql
@@ -1,17 +1,20 @@
 CREATE OR REPLACE FUNCTION directory."user.get"(
-    "@userNumber" text
+    "@userNumber" text,
+    "@actorId" int
 )
 RETURNS TABLE(
     "name" text,
+    "endUserNumber" varchar(20),
     "isSingleResult" boolean
 ) AS
 $BODY$
     SELECT
         "name",
+        "endUserNumber",
         true AS "isSingleResult"
     FROM
         directory.endUser
     WHERE
-        "endUserNumber" ="@userNumber"
+        ("endUserNumber" = "@userNumber" OR "actorId" = "@actorId")
 $BODY$
 LANGUAGE SQL
