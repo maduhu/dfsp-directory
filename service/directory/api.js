d387e917d976f16c126ff2991f1b27d30d54f174
stamen.peev <stamen.peev@softwaregroup-bg.com>
2016-11-15 13:53:11 +0200

    fix: add validations and move mock to external ocnfig

--- service/directory/api.js
+++ service/directory/api.js
@@ -4,6 +4,7 @@ module.exports = {
     // tags: ['tag1', 'tag2'],
     description: 'Lookup name, default account and currency of end user, given the end user number',
     notes: 'When the central directory service is asked to return information about the user (user.lookup API on the top level architect diagram), first it will check internally and find the URL of the default DFSP of the user, then it will send a call to this DFSP getting the information about the name of the user, default currency of the user and default account of the user (ILP address) and finally will return this information as a response to user.lookup API request.',
+    auth: false,
     params: joi.object({
       URI: joi.string().required().default('number:l1p')
     }),
@@ -12,5 +13,21 @@ module.exports = {
       account: joi.string().required(),
       currency: joi.string().required()
     })
+  },
+  'user.add': {
+    // tags: ['tag1', 'tag2'],
+    description: '',
+    notes: '',
+    auth: false,
+    params: joi.any(),
+    result: joi.any()
+  },
+  'user.remove': {
+    // tags: ['tag1', 'tag2'],
+    description: '',
+    notes: '',
+    auth: false,
+    params: joi.any(),
+    result: joi.any()
   }
 }
