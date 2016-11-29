9c4df1b446680a647917c1cb56292793d69061bd
stamen.peev <stamen.peev@softwaregroup-bg.com>
2016-11-29 15:10:55 +0200

    fix: add 'user not found' error

--- service/directory/index.js
+++ service/directory/index.js
@@ -1,7 +1,14 @@
 var path = require('path')
+var error = require('./error')
 module.exports = {
   schema: [{
     path: path.join(__dirname, 'schema'),
     linkSP: true
-  }]
+  }],
+  'user.get.response.receive': function(msg, $meta) {
+    if (Array.isArray(msg) && !msg.length) {
+      throw error.userNotFound()
+    }
+    return msg
+  }
 }
