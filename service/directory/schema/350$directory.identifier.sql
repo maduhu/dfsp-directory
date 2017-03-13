2416463b11ad28387d738690b681260aa28208a5
Radoslav Viktorov <Radoslav.viktorov@Softwaregroup-bg.com>
2017-03-13 19:16:40 +0200

    fix: add identifier table and rework user functions

---
+++ service/directory/schema/350$directory.identifier.sql
@@ -0,0 +1,9 @@
+CREATE TABLE directory.identifier(
+  "identifierId" SERIAL,
+  "identifier" VARCHAR(256),
+  "actorId" INTEGER,
+  "identifierTypeCode" VARCHAR(3),
+  CONSTRAINT "pkDirectoryIdentifier" PRIMARY KEY ("identifierId"),
+  CONSTRAINT "fkDirectoryIdentifier_directoryUser" FOREIGN KEY ("actorId") REFERENCES directory."user"("actorId"),
+  CONSTRAINT "fkDirectoryIdentifier_directoryIdentifierType" FOREIGN KEY ("identifierTypeCode") REFERENCES directory."identifierType"("code")
+)
--- service/directory/schema/750$directory.user.add.sql
+++ service/directory/schema/750$directory.user.add.sql
@@ -1,27 +1,35 @@
 CREATE OR REPLACE FUNCTION directory."user.add"(
-    "@userNumber" varchar(20),
-    "@firstName" varchar(255),
-    "@lastName" varchar(255),
-    "@dob" date,
-    "@nationalId" varchar(255)
+    "@identifier" VARCHAR(256),
+    "@identifierTypeCode" VARCHAR(3),
+    "@firstName" VARCHAR(255),
+    "@lastName" VARCHAR(255),
+    "@dob" DATE,
+    "@nationalId" VARCHAR(255)
 )
 RETURNS TABLE(
-    "actorId" integer,
-    "endUserNumber" varchar(20),
-    "firstName" varchar(255),
-    "lastName" varchar(255),
-    "dob" date,
-    "nationalId" varchar(255),
-    "isSingleResult" boolean
+    "actorId" INTEGER,
+    "@identifier" VARCHAR(20),
+    "@identifierTypeCode" VARCHAR(3),
+    "firstName" VARCHAR(255),
+    "lastName" VARCHAR(255),
+    "dob" DATE,
+    "nationalId" VARCHAR(255),
+    "isSingleResult" BOOLEAN
 ) AS
 $BODY$
     WITH u as (
-        INSERT INTO directory.endUser ("endUserNumber", "firstName", "lastName", "dob", "nationalId")
+        INSERT INTO directory.user ("firstName", "lastName", "dob", "nationalId")
         VALUES ("@userNumber", "@firstName", "@lastName", "@dob", "@nationalId")
         RETURNING *
     )
+
+    INSERT INTO directory.identifier ("identifier", "actorId", "identifierTypeCode")
+    VALUES ("@identifier", u."actorId", "@lastName", "@identifierTypeCode")
+
     SELECT
         *,
+        "@identifier" as "identifier",
+        "@identifierTypeCode" as "identifierTypeCode"
         true AS "isSingleResult"
     FROM u
 $BODY$
